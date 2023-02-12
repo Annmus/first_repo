b03339539b9b8f4251c52d226d853e1c1e3b979b
Anna D <anndoroslawska@gmail.com>
2023-02-12 10:40:14 +0100

    update

---
+++ selenium4.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+import datetime
+from selenium.webdriver.support import expected_conditions as excon
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.get('https://www.saucedemo.com/')
+
+def czekaj_na_id(element_id):
+    timeout = 10
+    timeout_message = f'Element o id {element_id} nie pojawil sie w czasie {timeout} s.'
+
+    lokalizator = ('id', element_id) #szukanie po id konkretnej wartości
+    znaleziono = excon.visibility_of_element_located(lokalizator) #patyk do dzgania strony
+    #obiekt bedzie pytany, czy jest ok, a odpowiedz bedzie zalezelc od tego, czy element jest widoczny
+    oczekiwator = WebDriverWait(driver, timeout)
+    return oczekiwator.until(znaleziono, timeout_message)
+
+def make_screenshot(driver):
+    teraz = datetime.datetime.now()
+    screenshot = 'screenshot' + teraz.strftime('_%H%M%S') + '.png'
+    driver.get_screenshot_as_file(screenshot)
+
+login_button = czekaj_na_id('login-button')
+
+make_screenshot(driver)
+time.sleep(2)
+driver.quit()
